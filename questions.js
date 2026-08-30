c76a1dfb9bce9bf85b7fa8939908eda39782a8d9
rgoswami64873-cmd <rgoswami64873@gmail.com>
2026-08-30 21:10:54 +0530

    update questions.js

---
+++ questions.js
@@ -0,0 +1,35 @@
+const testsDatabase = {
+    "test_01": [
+        {
+            subject: "Physics",
+            text: "दिए गए सर्किट में Equivalent Capacitance (C_eq) क्या होगा?",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Capacitors_in_parallel.svg/300px-Capacitors_in_parallel.svg.png", 
+            options: ["C1 + C2 + C3", "1/C1 + 1/C2 + 1/C3", "C1 * C2", "Zero"],
+            correct: 0
+        },
+        {
+            subject: "Biology",
+            text: "DNA के इस डायग्राम में 'X' लेबल वाले हिस्से को पहचानें:",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/DNA_chemical_structure.svg/300px-DNA_chemical_structure.svg.png",
+            options: ["Hydrogen Bond", "Phosphodiester Bond", "Glycosidic Bond", "Peptide Bond"],
+            correct: 1
+        },
+        {
+            subject: "Chemistry",
+            text: "विलयन (Solutions) में Raoult's Law किस पर लागू होता है?",
+            image: null,
+            options: ["Ideal Solutions", "Non-ideal Solutions", "Both", "None"],
+            correct: 0
+        }
+    ],
+    
+    "test_02": [
+        {
+            subject: "Biology",
+            text: "Sexual Reproduction in Flowering Plants में Double Fertilization किसने खोजा था?",
+            image: null,
+            options: ["Nawaschin", "Strasburger", "Leeuwenhoek", "Robert Hooke"],
+            correct: 0
+        }
+    ]
+};
